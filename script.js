8da584623cf8ad3ea85c70a04ad1934d54dddd90
Sathya Priya <sathyapriyababu91@gmail.com>
2026-03-15 23:13:18 +0530

    updated

--- script.js
+++ script.js
@@ -1,63 +1,91 @@
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-
-if(!SpeechRecognition){
-    alert("your browesr not support to speech recognition");
-}else{
-    const recognition = new SpeechRecognition();
-
-
-    recognition.continuous = false;
-    recognition.interimResults = false;
-    recognition.lang = "en-IN";
-
-    const output = document.getElementById("result");
     const input = document.getElementById("taskInput");
     const language = document.getElementById("language");
     const list = document.getElementById("taskList");
+    const output = document.getElementById("result");
     
     const start = document.getElementById("start-btn");
     const stop = document.getElementById("stop-btn");
+    const addbtn = document.getElementById("add-btn");
 
-    start.addEventListener("click", () =>{
-        
-        recognition.lang = language.value;
-        recognition.start();
-        output.innerText = "Listening.....";
-        start.classList.add("listening");
+    const counter = document.getElementById("counter");
+
+
+    let recognition;
+
+    if(!SpeechRecognition){
+    alert("your browser not support speach recognition");
+    }else{
+      recognition = new SpeechRecognition();
+    
+    recognition.continuous = true;
+    recognition.interimResults = false;
+
+
+    start.addEventListener("click" , () => {
+    recognition.lang = language.value;
+
+    recognition.start();
+    output.innerText = "🎤 Listening...";
+
     });
 
-    stop.addEventListener("click" , () => {
+    stop.addEventListener("click", () => {
         recognition.stop();
-        start.classList.remove("listening");
+        output.innerText = "Stopped.......";
+
     });
 
-    recognition.onresult = (event) => {
-     const transcript = event.results[0][0].transcript.trim();
-    
-    output.innerText = transcript;
-    input.value = transcript;
-    
+recognition.onresult = (event) => {
 
-    const li = document.createElement("li");
+const transcript = event.results[event.results.length - 1][0].transcript.trim();
 
-        li.innerHTML = `${transcript}
-        <button class="delete-btn">❌</button>`;
-    
-    
-    list.appendChild(li);
+input.value = transcript;
+addTask(transcript);
 
-        const deleteBtn = li.querySelector(".delete-btn");
-
-    deleteBtn.addEventListener("click", () => {
-        li.remove();
-
-
-
-});
-};       
-    recognition.onerror = (event) => {
-        output.innerText = "Error: " + event.error;
 
 };
 }
+
+function addTask(task){
+
+if(task === "") return;
+
+const li = document.createElement("li");
+
+li.innerHTML = `
+<span>${task}</span>
+<div>
+<input type="checkbox" class="check">
+<button class="delete"><i class="bi bi-trash3"></i>
+</button>
+</div>
+`;
+
+list.appendChild(li);
+
+input.value="";
+
+const check=li.querySelector(".check")
+const text=li.querySelector("span")
+const del=li.querySelector(".delete")
+
+check.addEventListener("change", () =>{
+    text.classList.toggle("completed");
+});
+
+
+del.addEventListener("click", () =>{
+    li.remove();
+});
+}
+addbtn.addEventListener("click" , () =>{
+    addTask(input.value);
+});
+
+input.addEventListener("keypress",(e)=>{
+    if(e.key==="Enter"){
+        addTask(input.value);
+    }
+});
